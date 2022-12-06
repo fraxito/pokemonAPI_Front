a6afe74919603cbb358a1621a297548758098f20
Jorge Cisneros <fraxito@gmail.com>
2022-12-07 00:05:51 +0100

    Initial commit

---
+++ src/main/java/org/vaadin/example/MainView.java
@@ -0,0 +1,70 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and use @Route
+ * annotation to announce it in a URL as a Spring managed bean.
+ * <p>
+ * A new instance of this class is created for every new user and every browser
+ * tab/window.
+ * <p>
+ * The main view contains a text field for getting the user name and a button
+ * that shows a greeting message in a notification.
+ */
+@Route
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+
+    /**
+     * Construct a new Vaadin view.
+     * <p>
+     * Build the initial UI state for the user accessing the application.
+     *
+     * @param service
+     *            The message service. Automatically injected Spring managed
+     *            bean.
+     */
+    public MainView(@Autowired PokemonService service) {
+        HorizontalLayout inputs = new HorizontalLayout();
+        VerticalLayout results = new VerticalLayout();
+        ComboBox<String> comboBox = new ComboBox<>("Browser");
+        comboBox.setAllowCustomValue(false); //este deja que el usuario escriba lo que quiera en la caja del comboBox. Si se pone a false no deja
+        comboBox.setItems("people", "planets", "starships");
+        comboBox.setHelperText("Selecciona el tipo de petición");
+
+        TextField requestId = new TextField("Request id");
+        requestId.addThemeName("bordered");
+        inputs.add(comboBox, requestId);
+        Button boton1 = new Button("Lee caracter",
+                e -> {
+                    String tipo = comboBox.getValue();
+                    int id = Integer.parseInt(requestId.getValue());
+                    try {
+                        System.out.println(service.leePokemon(tipo,id));
+                    } catch (Exception ex) {
+                    }
+                });
+        boton1.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        boton1.addClickShortcut(Key.ENTER);
+        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
+        addClassName("centered-content");
+
+        add(inputs, boton1);
+    }
+
+}
---
+++ src/main/java/org/vaadin/example/PokemonService.java
@@ -0,0 +1,14 @@
+package org.vaadin.example;
+
+import java.io.Serializable;
+
+public class PokemonService implements Serializable {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+}
